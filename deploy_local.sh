a5983dd49595236c7fd2f49c0789f549cbf4a199
SBJakobsen <sorenbj1234@hotmail.com>
2023-05-04 15:09:37 +0200

    Functioning gdk component build/publish

---
+++ deploy_local.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 1. GDK component build
+
+# 2. Run this
+sudo /greengrass/v2/bin/greengrass-cli deployment create \
+  --recipeDir /home/soren/projects/aws-greengrass-kvs-connector/greengrass-build/recipes \
+  --artifactDir /home/soren/projects/aws-greengrass-kvs-connector/greengrass-build/artifacts \
+  --update-config /home/soren/projects/aws-greengrass-kvs-connector/config.json \
+  --merge "Blackbird.kvs.connector=1.0.0"
+
+# 3. systemctl restart greengrass for changes to take effect
